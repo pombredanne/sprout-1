088e402ba3b6368902fd30eff1d1750d2deb8f27
Tony Rasa <trasa@meancat.com>
2013-10-08 12:27:39 -0700

    clean up tmp dir and make sure it exists before downloading artifacts

--- sprout/fabfile.py
+++ sprout/fabfile.py
@@ -1,12 +1,40 @@
 from fabric.api import local
 import json
+import os
+import glob
 from pprint import pprint
-from sprout import config
+from sprout import * 
 
 __all__ = ['get_artifacts', 'deploy']
 
+
+def _clean(tmpdir):
+    """ Remove any existing files from a previous run of sprout. """
+    _ensure_exists(tmpdir)
+    _remove_files(tmpdir)
+        
+
+def _ensure_exists(tmpdir):
+    try:
+        os.makedirs(tmpdir)
+    except OSError:
+        if not os.path.isdir(tmpdir):
+            raise
+        
+def _remove_files(tmpdir):
+    files = glob.glob(os.path.join(tmpdir, '*'))
+    for f in files:
+        print('removing %s' % f)
+        os.remove(f)
+
+
 def get_artifacts(config_file):
     c = config.load_config(config_file)
+    _clean(cfg.settings['local_temp_dir'])
+
+    # for each artifact, build a url to nexus
+    # get that artifact and put it in the local store
+
 
 
 def deploy(config_file):
